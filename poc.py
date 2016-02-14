ca754736b002e7098410eedf3c9aeaa7f33febf7
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2016-02-15 00:09:32 +0100

    poc: bybye df, we don't need you for the heatmap

--- poc.py
+++ poc.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python
 
 import btrfs
-import fcntl
 import os
-
-
-def df(fd):
-    space_args = btrfs.sized_array(btrfs.ioctl_space_args.size)
-    fcntl.ioctl(fd, btrfs.IOC_SPACE_INFO, space_args)
-    _, total_spaces = btrfs.ioctl_space_args.unpack(space_args)
-    space_info_buf_size = btrfs.ioctl_space_args.size + btrfs.ioctl_space_info.size * total_spaces
-    space_info = btrfs.sized_array(space_info_buf_size)
-    btrfs.ioctl_space_args.pack_into(space_info, 0, total_spaces, 0)
-    fcntl.ioctl(fd, btrfs.IOC_SPACE_INFO, space_info)
-    for offset in xrange(btrfs.ioctl_space_args.size,
-                         space_info_buf_size,
-                         btrfs.ioctl_space_info.size):
-        flags, total, used = btrfs.ioctl_space_info.unpack_from(space_info, offset)
-        print("flags: %s, size: %s, used: %s" % (flags, total, used))
 
 
 def devices(fd):
@@ -61,7 +45,6 @@ def block_group_used_for_chunk(fd, vaddr):
 
 def main():
     fd = os.open("/mnt/heatmap", os.O_RDONLY)
-    df(fd)
     devices(fd)
     chunks(fd)
     os.close(fd)
