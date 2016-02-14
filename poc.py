071ec796709cf3fd06c161cacb093283c5257b17
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2016-02-14 23:33:14 +0100

    poc: merge block group used into chunk output

--- poc.py
+++ poc.py
@@ -40,22 +40,23 @@ def chunks(fd):
         num_stripes = chunk[7]
         pos = btrfs.chunk.size
         vaddr = header[2]
+        used = block_group_used_for_chunk(fd, vaddr)
         for i in xrange(num_stripes):
             stripe = btrfs.stripe.unpack_from(buf, pos)
             pos += btrfs.stripe.size
-            print("chunk type %s stripe %s devid %s offset %s length %s vaddr %s" %
-                  (chunk[3], i, stripe[0], stripe[1], chunk[0], vaddr))
-        block_group_for_chunk(fd, vaddr)
+            print("chunk type %s stripe %s devid %s offset %s length %s used %s" %
+                  (chunk[3], i, stripe[0], stripe[1], chunk[0], used))
 
 
-def block_group_for_chunk(fd, vaddr):
+def block_group_used_for_chunk(fd, vaddr):
     block_groups = btrfs.search(fd,
                                 tree=btrfs.EXTENT_TREE_OBJECTID,
                                 objid=vaddr,
                                 key_type=btrfs.BLOCK_GROUP_ITEM_KEY,
                                 structure=btrfs.block_group_item)
-    for header, buf, block_group in block_groups:
-        print("block group used %s" % block_group[0])
+    if len(block_groups) > 1:
+        raise Exception("Apparently I have no idea what I'm doing!")
+    return block_groups[0][2][0]
 
 
 def main():
